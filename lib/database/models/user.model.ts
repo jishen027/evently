631684a9b082e8680a4336b61274c2d6ee0d3d77
Jeb Lee <jishen027@gmail.com>
2024-03-06 15:42:17 +0000

    Remove name field from UserSchema

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -11,10 +11,6 @@ export const UserSchema = new Schema({
     required: true,
     unique: true,
   },
-  name: {
-    type: String,
-    required: true,
-  },
   username: {
     type: String,
     required: true,
